c0d973ffae5c331a5d577b957cd44b4585e4b442
Syz03 <aakibdiscoo@gmail.com>
2021-06-07 15:42:16 +0545

    Updates with index.js in 4th chall

---
+++ HTML & CSS/Junior/clipboard-landing-page-master/index.js
@@ -0,0 +1,10 @@
+
+// iosBtn.addEventListener('click', animateBtn)
+
+
+function animateBtn (){
+    var iosBtn = document.querySelector('.ios-btn');
+    iosBtn.classList.toggle('animation');
+}
+
+
